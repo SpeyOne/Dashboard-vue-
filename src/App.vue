33eafbf9cd43c4811aed911b825c19a82443b7f0
SpeyOne <96263351+SpeyOne@users.noreply.github.com>
2022-08-02 20:31:54 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <PageHeader/>
+    <NavBarVF/>
+    <DashBoard/>
+    <WavyChart/>
+    <CrunchBoard/>
+    <ThreeRowChart/>
+    <CrunchNumbers/>
+    <ProgressBoard/>
+    <ProgressBars/>
+    <BottomCharts/>
+    <PieChart/>
+    <GeneralMenu/>
+    <TotalOverdue/>
+  </div>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue'
+import NavBarVF from './components/NavBarVF.vue'
+import DashBoard from './components/DashBoard.vue'
+import WavyChart from './components/WavyChart.vue'
+import CrunchBoard from './components/CrunchBoard.vue'
+import ThreeRowChart from './components/ThreeRowChart.vue'
+import CrunchNumbers from './components/CrunchNumbers.vue'
+import ProgressBoard from './components/ProgressBoard.vue'
+import ProgressBars from './components/ProgressBars.vue'
+import BottomCharts from './components/BottomCharts.vue'
+import PieChart from './components/PieChart.vue'
+import GeneralMenu from './components/GeneralMenu.vue'
+import TotalOverdue from './components/TotalOverdue.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    NavBarVF,
+    DashBoard,
+    WavyChart,
+    CrunchBoard,
+    ThreeRowChart,
+    CrunchNumbers,
+    ProgressBoard,
+    ProgressBars,
+    BottomCharts,
+    PieChart,
+    GeneralMenu,
+    TotalOverdue
+  }
+}
+</script>
+
+<style>
+#app {
+  min-width: 1040px;
+  margin: 0;
+  padding: 0;
+  min-height: 410vh;
+  font-family: "Myriad Pro", 'Myriad', "Liberation Sans", "Nimbus Sans L", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  color: #404040;
+  background-color: #f5f5f5;
+}
+</style>
---
+++ src/components/BottomCharts.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="bottom_charts">
+   <div class="title">
+                <p><strong>Crunch some Numbers</strong><br>See how your projects are progressing via the new statistics engine.</p>
+                <div class="timeline_selector">
+                  <label>Timeline:</label>
+                  <select class="timeline" id="timeline" name="timeline_selector">
+                    <option value="first">3 Days</option>
+                    <option selected value="second">7 Days</option>
+                    <option value="third">Month</option>
+                  </select>
+                </div>
+              </div>
+              <div class="profile_stats">
+              </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.bottom_charts {
+    font-family: 'Avenir Next Cyr', sans-serif;
+    margin-left: 300px;
+    width: 950px;
+    background-color: #f5f5f5;
+      margin-bottom: 25px;
+}
+
+.profile_stats {
+  display: flex;
+  flex-direction: row;
+}
+
+.title {
+  margin: 50px 0 20px 0;
+  text-align: left;
+  border-bottom: 2px solid #dee3e7;
+}
+
+.title p {
+  color: #78909c;
+  font-size: 14px;
+  line-height: 21px;
+}
+
+.title strong {
+  color: #37474f;
+  font-size: 20px;
+  font-weight: normal;
+}
+
+</style>
